7b6724818567719257d596d2b510174748ebc427
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-04-14 17:27:15 +0300

    Create Tests/DonationTests.cs

---
+++ Tests/DonationTests.cs
@@ -0,0 +1,1 @@
+using NUnit.Framework;\nusing System.Net;\nusing System.Net.Http;\nusing System.Threading.Tasks;\n\n[TestFixture]\npublic class DonationTests\n{\n    private DonationApiClient _client;\n\n
