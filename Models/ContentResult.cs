30d9ba1bda4ac74464fd1c948af8574992cffb73
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-04-14 15:59:54 +0300

    Create Models/ContentResult.cs

---
+++ Models/ContentResult.cs
@@ -0,0 +1,9 @@
+namespace DonorApp.Services.DTO
+{
+    public class ContentResult
+    {
+        public string Content { get; set; }
+        public string ContentType { get; set; }
+        public int? StatusCode { get; set; }
+    }
+}
