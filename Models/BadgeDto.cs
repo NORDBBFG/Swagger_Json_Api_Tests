c24817cf2a49dc94158dbd39f1db513be251521b
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-04-16 15:56:23 +0300

    Update Models/BadgeDto.cs

--- Models/BadgeDto.cs
+++ Models/BadgeDto.cs
@@ -2,19 +2,14 @@ using System;
 
 public class BadgeDto
 {
-    public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string Picture { get; set; }
-    public DateTime CreateDate { get; set; }
-    public string CreatedBy { get; set; }
-    public DateTime UpdateDate { get; set; }
-    public string LastUpdatedBy { get; set; }
+    public string IconUrl { get; set; }
 }
 
 public class ContentResult
 {
-    public string Content { get; set; }
-    public string ContentType { get; set; }
-    public int? StatusCode { get; set; }
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
 }
