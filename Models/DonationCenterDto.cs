b10864dbe244b4ba2564d05710eb8a2db7f6af21
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-04-18 17:38:06 +0300

    Create Models/DonationCenterDto.cs

---
+++ Models/DonationCenterDto.cs
@@ -0,0 +1,10 @@
+public class DonationCenterDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int RegionId { get; set; }
+    public int CityId { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
