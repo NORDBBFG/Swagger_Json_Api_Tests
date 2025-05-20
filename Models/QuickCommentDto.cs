7155126df2936ad4c12bf9e19043ed0f205e2c2a
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-05-20 19:06:16 +0300

    Create Models/QuickCommentDto.cs

---
+++ Models/QuickCommentDto.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class QuickCommentDto
+{
+    public int Id { get; set; }
+    public string Comment { get; set; }
+    public int UserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
