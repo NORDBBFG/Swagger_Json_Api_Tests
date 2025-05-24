8c411b6b1785d2e1481e7a593a639b28afa4ffe1
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-05-24 14:06:26 +0300

    Create Models/DonationTypeDto.cs

---
+++ Models/DonationTypeDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DonorApp.Services.DTO
+{
+    public class DonationTypeDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Add other properties as needed
+    }
+}
