358e9ebfd5cad5965476f6a061048c8c06356cbc
NORDBBFG <79658195+NORDBBFG@users.noreply.github.com>
2025-05-26 15:30:44 +0300

    Create Models/FaqCategoryDto.cs

---
+++ Models/FaqCategoryDto.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class FaqCategoryDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Picture { get; set; }
+    public int FaqArticlesCount { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public string CreatedBy { get; set; }
+    public string LastUpdatedBy { get; set; }
+    public string CreatedByName { get; set; }
+    public string LastUpdatedByName { get; set; }
+    public bool IsPermanent { get; set; }
+}
